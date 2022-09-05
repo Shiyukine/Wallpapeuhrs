11f37f50568708bad27d1767ccee18e4066608c2
Shiyukine <benjbres@gmail.com>
2022-09-05 19:46:27 +0200

    removed useless windows

--- Wallpapeuhrs/Program.cs
+++ Wallpapeuhrs/Program.cs
@@ -11,7 +11,7 @@ namespace Wallpapeuhrs
         [System.STAThreadAttribute()]
         public static void Main(string[] args)
         {
-            if (args.Length > 0 && args[0].Contains("wp") && Convert.ToInt32(args[6]) == 1) new WallpapeuhrsBG.App();
+            if (args.Length > 0 && args[0].Contains("wp") && Convert.ToInt32(args[6]) > 3) new WallpapeuhrsBG.App();
             App.nargs = args;
             var app = new App();
             app.InitializeComponent();
