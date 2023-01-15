5d46962ce3b838f358c33158bb34eab1db26fdb7
Shiyukine <benjbres@gmail.com>
2023-01-15 01:59:49 +0100

    optimized file resources

--- Wallpapeuhrs/MediaVW.xaml.cs
+++ Wallpapeuhrs/MediaVW.xaml.cs
@@ -11,7 +11,6 @@ using System.Windows.Controls;
 using System.Windows.Interop;
 using Wallpapeuhrs.Utils;
 using System.Web;
-using Microsoft.AspNetCore.StaticFiles;
 
 namespace Wallpapeuhrs
 {
@@ -44,36 +43,6 @@ namespace Wallpapeuhrs
                 {
                     //if (System.Windows.Forms.Screen.PrimaryScreen.DeviceName == parent.moni) webview.CoreWebView2.OpenDevToolsWindow();
                     string result = reader.ReadToEnd();
-                    webview.CoreWebView2.AddWebResourceRequestedFilter("*", CoreWebView2WebResourceContext.All);
-                    webview.CoreWebView2.WebResourceRequested += async (s, e) =>
-                    {
-                        if (e.Request.Uri.StartsWith("https://myfiles/"))
-                        {
-                            var uri = new Uri(e.Request.Uri);
-                            var fileName = uri.AbsolutePath.Substring(1);
-                            var deferral = e.GetDeferral();
-                            try
-                            {
-                                Stream res = await getResource(fileName);
-                                if (res != null)
-                                {
-                                    e.Response = webview.CoreWebView2.Environment.CreateWebResourceResponse(res, 200, "OK", null);
-                                    e.Response.Headers.AppendHeader("Access-Control-Allow-Origin", "*");
-                                    e.Response.Headers.AppendHeader("Access-Control-Allow-Headers", "*");
-                                    e.Response.Headers.AppendHeader("Accept-Ranges", "bytes");
-                                }
-                                else e.Response = webview.CoreWebView2.Environment.CreateWebResourceResponse(new MemoryStream(Encoding.UTF8.GetBytes("Unable to load \"" + fileName + "\"")), 404, "Not found", null);
-                            }
-                            catch (Exception ex)
-                            {
-                                e.Response = webview.CoreWebView2.Environment.CreateWebResourceResponse(new MemoryStream(Encoding.UTF8.GetBytes("Unable to load \"" + fileName + "\"")), 500, "Internal Server Error", null);
-                            }
-                            string contentType;
-                            new FileExtensionContentTypeProvider().TryGetContentType(fileName, out contentType);
-                            e.Response.Headers.AppendHeader("Content-type", contentType);
-                            deferral.Complete();
-                        }
-                    };
                     webview.NavigationCompleted += async (s, e) =>
                     {
                         webview.CoreWebView2.AddHostObjectToScript("boundobject", new ChromeBoundObject(parent));
@@ -94,6 +63,7 @@ namespace Wallpapeuhrs
                 if (renderer == 2) args += "--use-angle=d3d11 ";
                 if (renderer == 3) args += "--use-angle=d3d11on12 ";
                 args += "--disable-gpu-vsync ";
+                args += "--disable-web-security ";
                 opt.AdditionalBrowserArguments = args;
                 /*actual 
                  * opt.AdditionalBrowserArguments = "--disable-features=HardwareMediaKeyHandling " +
@@ -148,28 +118,6 @@ namespace Wallpapeuhrs
             main.ManipulationMode = Windows.UI.Xaml.Input.ManipulationModes.None;*/
         }
 
-        public static async Task<Stream> getResource(string path)
-        {
-            return await Task.Run(() =>
-            {
-                try
-                {
-                    StreamReader sr = new StreamReader(HttpUtility.UrlDecode(path));
-                    Stream stream = sr.BaseStream;
-                    async void flush()
-                    {
-                        await sr.BaseStream.FlushAsync();
-                    }
-                    flush();
-                    return stream;
-                }
-                catch (Exception e)
-                {
-                    return null;
-                }
-            });
-        }
-
         private void setDWM(IntPtr handle)
         {
             int a = 1;
@@ -189,7 +137,6 @@ namespace Wallpapeuhrs
         public async void changeUrl(string newUrl)
         {
             curUrl = newUrl;
-            newUrl = "https://myfiles/" + newUrl;
             parent.log("aaaaaaaa " + newUrl + " " + System.IO.Path.GetExtension(newUrl));
             foreach (string ext in App.types.Keys)
             {
